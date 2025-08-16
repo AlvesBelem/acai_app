91b3ea11dc25c5e8edd44635ce4f08c96686fcb4
AlvesBelem <marcelocartoes.pa@gmail.com>
2025-08-16 16:51:45 -0300

    refatorando

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,48 +1,15 @@
-import golden from '../../../public/imagens/golden.png'
-import royal from '../../../public/imagens/royal.png'
-import primier from '../../../public/imagens/primier.png'
-import whiskas from '../../../public/imagens/whiskas.png'
-import natural from '../../../public/imagens/natural.png'
-import Image from 'next/image'
+
 import { WhatsappLogoIcon, FacebookLogoIcon, InstagramLogoIcon, XLogoIcon, YoutubeLogoIcon } from '@phosphor-icons/react/dist/ssr'
 import { Heart } from 'lucide-react'
 
 
-const brands = [
-    { name: "Royal Canin", logo: royal },
-    { name: "Golden", logo: golden },
-    { name: "Primier", logo: primier },
-    { name: "Formula Natural", logo: natural },
-    { name: "Whiskas", logo: whiskas },
-    { name: "Golden", logo: golden },
-]
+
 
 
 export function Footer() {
     return (
         <section className='bg-[#620F83] py-16 text-white'>
             <div className='container mx-auto px-4'>
-                {/* <div className='border-b border-white/20 pb-8'>
-                    <h4 data-aos="fade-up-left" data-aos-delay="6000" className='text-3xl font-semibold mb-8 text-center'>Marcas que trabalhamos</h4>
-                    <div data-aos="fade-up-left" data-aos-delay="6500" className='grid grid-cols-2 lg:grid-cols-6 gap-8'>
-                        {brands.map((item, index) => (
-                            <div key={index} className='bg-white p-4 rounded-lg flex items-center justify-center'>
-                                <Image
-                                    src={item.logo}
-                                    alt={item.name}
-                                    width={100}
-                                    height={50}
-                                    className='object-contain'
-                                    quality={100}
-                                    style={{
-                                        width: 'auto',
-                                        height: 'auto',
-                                    }}
-                                />
-                            </div>
-                        ))}
-                    </div>
-                </div> */}
                 <footer data-aos="fade-up-left" data-aos-delay="7000" className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-12 mt-5 text-center'>
                     <div className='flex flex-col items-center'>
                         <h3 className='text-2xl font-semibold mb-2'>Açai da Amazônia</h3>
